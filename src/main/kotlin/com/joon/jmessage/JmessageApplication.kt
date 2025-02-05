ba06c1ee47cd504e9faaa43405da08e5f757e181
jy.ahn <jy.ahn@linecorp.com>
2025-02-05 17:59:04 +0900

    Init project

---
+++ src/main/kotlin/com/joon/jmessage/JmessageApplication.kt
@@ -0,0 +1,11 @@
+package com.joon.jmessage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JmessageApplication
+
+fun main(args: Array<String>) {
+	runApplication<JmessageApplication>(*args)
+}
---
+++ src/test/kotlin/com/joon/jmessage/JmessageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joon.jmessage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JmessageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
